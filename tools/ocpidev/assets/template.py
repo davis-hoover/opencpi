7b58bd37d5fcba1401d8b7ba2b43c4faf97472ad
Connor Lathrop <connor.lathrop@cnftech.com>
2020-01-06 20:52:54 +0000

    #675 Fixes issue where new projects couldn't be imported into IDE.

--- tools/ocpidev/assets/template.py
+++ tools/ocpidev/assets/template.py
@@ -70,8 +70,7 @@ PROJ_GIT_ATTR = """
 *.bit -diff
 \n\n"""
 
-PROJ_PROJECT_MK ="""
-# This Makefile fragment is for the {{name}} project
+PROJ_PROJECT_MK ="""# This Makefile fragment is for the {{name}} project
 
 # Package identifier is used in a hierarchical fashion from Project to Libraries....
 # The PackageName, PackagePrefix and Package variables can optionally be set here:
@@ -110,15 +109,31 @@ ComponentLibraries={{comp_lib}}
 {% endif %}
 \n\n"""
 
-PROJ_MAKEFILE= ("""
+PROJ_MAKEFILE= ("""# This file is protected by Copyright. Please refer to the COPYRIGHT file
+# distributed with this source distribution.
+#
+# This file is part of OpenCPI <http://www.opencpi.org>
+#
+# OpenCPI is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# OpenCPI is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
 $(if $(realpath $(OCPI_CDK_DIR)),,\\
   $(error The OCPI_CDK_DIR environment variable is not set correctly.))
 # This is the Makefile for the {{name}} project.
 include $(OCPI_CDK_DIR)/include/project.mk
-\n\n""")
+\n""")
 
-PROJ_GUI_PROJECT = ("""
-<?xml version="1.0" encoding="UTF-8"?>
+PROJ_GUI_PROJECT = ("""<?xml version="1.0" encoding="UTF-8"?>
 <projectDescription>
   <name>{{determined_package_id}}</name>
   <comment></comment>
