c3f15800d82e2a10c2a0b3a7fb95215e52a177de
Nathan Knowles <nknowles88@gmail.com>
2020-01-11 22:24:42 -0600

    fix update-release.sh
    [skip ci]

--- scripts/update-release.sh
+++ scripts/update-release.sh
@@ -6,19 +6,20 @@
 set -e
 # Fix the common release subtitle and any URLs in fodt files
 (cd doc/odt
- sed "s|\(OpenCPI Release \)[^<]*|\1$1|"< shared/release.fodt > shared/release.fodt.rel
+ sed -i "s|\(OpenCPI Release \)[^<]*|\1$1|" shared/release.fodt
  for f in *.fodt; do
-     sed 's|\( xlink:href="https://opencpi.gitlab.io/releases/\)[^/]*\(/docs/[^"]*"\)|\1'$1'\2|g' $f > $f.rel
+     sed -i 's|\( xlink:href="https://opencpi.gitlab.io/releases/\)[^/]*\(/docs/[^"]*"\)|\1'$1'\2|g' $f
+     sed -i 's|\(>https://opencpi.gitlab.io/releases/\)[^/]*\(/docs/[^<]*<\)|\1'$1'\2|g' $f
  done
 )
 # Fix the common release subtitle for (some) tex docs
 (cd doc/av/tex/snippets
- sed "s/\(ocpiversion{\)[^}]*/\1$1/" includes.tex > includes.tex.rel
+ sed -i "s/\(ocpiversion{\)[^}]*/\1$1/" includes.tex
 )
 # Fix the builtin software release tag for compatibility checking
 # Uses arg 2 which is a release tag without any suffixes etc.
 (cd build/autotools
- base=$(echo $1 | sed 's/-.*//')
+ base=$(echo ${1#v*} | sed 's/-.*//')
  major=$(echo $base | sed 's/^\([^.]*\).*$/\1/')
  minor=$(echo $base | sed 's/^[^.]*\.\([^.]*\).*$/\1/')
  patch=$(echo $base | sed 's/^[^.]*\.[^.]*\.\([^.]*\).*$/\1/')
@@ -26,5 +27,5 @@ set -e
      -e "s/\(AC_DEFINE(\[VERSION_MAJOR\],\)[^,]*\(,.*\)$/\1$major\2/" \
      -e "s/\(AC_DEFINE(\[VERSION_MINOR\],\)[^,]*\(,.*\)$/\1$minor\2/" \
      -e "s/\(AC_DEFINE(\[VERSION_PATCHLEVEL\],\)[^,]*\(,.*\)$/\1$patch\2/" \
-     configure.ac > configure.ac.rel
+     -i configure.ac
 )
