f5638c53b414a3b78c503cbe01ff70a1c14d427a
Nathan Knowles <nathan.knowles@cnftech.com>
2020-01-26 17:23:26 -0600

    (#906) Update dockerfile, rewrite install-opencpi-docs.sh

--- scripts/install-opencpi-docs.sh
+++ scripts/install-opencpi-docs.sh
@@ -18,30 +18,31 @@
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 
 ################################################################################
-# This script installs packages required to build OpenCPI documentation and then
-# builds the documents in the framework and builtin projects, both tex and odt.
-# It does not depend on the framework or projects being built, although it usually
-# runs after install-opencpi.sh
-# It currently is only supported on centos7
 
-# Just check if it looks like we are in the source tree.
-[ -d runtime -a -d build -a -d scripts -a -d tools ] || {
-  echo "Error:  this script ($0) is not being run from the top level of the OpenCPI source tree."
-  exit 1
-}
 set -e
 
-# We do some bootstrapping here (that is also done in the scripts we call), in order to know
-# whether the platform we are building
+function usage {
+  echo "\
+This script installs packages required to build OpenCPI documentation and then
+builds the documents in the framework and builtin projects, both tex and odt.
+It does not depend on the framework or projects being built, although it usually
+runs after install-opencpi.sh
 
-# Ensure exports (or cdk) exists and has scripts
-source ./scripts/init-opencpi.sh
+This script is only supported on centos7.
 
-# Ensure CDK and TOOL variables
-OCPI_BOOTSTRAP=`pwd`/cdk/scripts/ocpibootstrap.sh; source $OCPI_BOOTSTRAP
+Usage: $(basename $0) [--no-build]
 
-[ "${OCPI_TOOL_PLATFORM}" == "centos7" ] || (echo "This script only works on CentOS 7, sorry!"; exit 1)
-sanity=--setopt=skip_missing_names_on_install=False
+  --no-build  Only install packages
+"
+  exit 1
+}
+
+# Parse args
+case "$1" in
+  --no-build)  no_build=1 ;;
+  '') ;;
+  *)  usage ;;
+esac
 
 # Docker doesn't have sudo installed by default and we run as root inside
 # a container anyway
@@ -53,18 +54,33 @@ Error: Could not find 'sudo' and you are not root. Installing packages requires
 root permissions." && exit 1
 fi
 
-echo Installing all the standard packages required to build OpenCPI
-echo documentation using "$SUDO yum install"...
+if ! grep -q 'CentOS Linux release 7' /etc/redhat-release ; then
+  echo 'This sript only works on CentOS 7, sorry!'
+  exit 1
+fi
+
+echo 'Installing all the standard packages required to build OpenCPI'
+echo "documentation using '$SUDO yum install'..."
+sanity=--setopt=skip_missing_names_on_install=False
 $SUDO yum install -y $sanity epel-release
 $SUDO yum install -y $sanity ghostscript git libreoffice-writer make \
   python34 python34-jinja2 rubber texlive texlive-appendix texlive-latex \
   texlive-multirow texlive-placeins texlive-titlesec texlive-xstring \
   https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
+echo 'Packages required to build OpenCPI documentation have been installed.'
 
-echo Packages required to build OpenCPI documentation have been installed.
-if [ "$1" = "--no-build" ]; then
-  echo To build OpenCPI documentation, run '`make doc`'.
-else
-  echo 'Building documentation using `make doc`'
-  make doc
+if [ -n "$no_build" ]; then
+  echo 'To build OpenCPI documentation, run `make doc`.'
+  exit 0
 fi
+
+# Just check if it looks like we are in the source tree.
+[ -d runtime -a -d build -a -d scripts -a -d tools ] || {
+  echo "Error:  this script ($0) is not being run from the top level of the OpenCPI source tree."
+  exit 1
+}
+
+# Ensure exports (or cdk) exists and has scripts
+source ./scripts/init-opencpi.sh
+echo 'Building documentation using `make doc`.'
+make doc
