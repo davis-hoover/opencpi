b7ec0fdcbf3ee6c9064201ff19216a4363c59955
jekulp <jek@parera.com>
2020-03-15 13:39:46 -0400

    add new files for parameterized AXI interfaces

---
+++ projects/core/hdl/primitives/axi/axi_interface_zynq7_sgp.h
@@ -0,0 +1,5 @@
+// Define the zynq 7 series S_AXI_GP AXI interface
+#define ADDR_WIDTH 32
+#define ID_WIDTH 6
+#define DATA_WIDTH 32
+#define CLOCK_FROM_MASTER 1
---
+++ projects/core/hdl/primitives/axi/axi_interface_zynqMP_mgp.h
@@ -0,0 +1,16 @@
+// Define the zynq MP/ultrascale series M_AXI_GP AXI interface
+// the OpenCPI VHDL wrapper module zynq_ultra_ps_e:
+//   - has the (generated here) record interface
+//   - wraps/instantiates the underlying VHDL interface zynq_ultra_ps_e_0
+//   - does not change any generic parameters of zynq_ultra_ps_e_0
+// the Xilinx VHDL wrapper component/entity zynq_ultra_ps_e_0:
+//   - was generated from the GUI or TCL
+//   - instantiates the underlying Verilog wrapper zynq_ultra_ps_e_v3_3_0_zynq_ultra_ps_e
+//   - overrides the M_AXI_GP* default data wdith (128) to 32 (we only use GP0)
+//   - overrides the S_AXI_GP1-6 default data width (128) to 64 (which opencpi does not use)
+//   - sets other processor stuff - it represents all the processor options from generation
+// Can't we avoid the extra VHDL wrapper in any case?
+#define ADDR_WIDTH 40
+#define ID_WIDTH 16
+#define DATA_WIDTH 32 // The hardware can do 128, but we should do 64 when the cp interface is parameterized
+#define RESET_FROM_MASTER 1
