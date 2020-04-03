b470c1a6cf1a2aa2b49693396b5d165404dece74
jekulp <jek@parera.com>
2020-04-03 17:27:45 -0400

    add c++ flags to C too

--- projects/core/rcc/platforms/xilinx19_2_aarch64/xilinx19_2_aarch64.mk
+++ projects/core/rcc/platforms/xilinx19_2_aarch64/xilinx19_2_aarch64.mk
@@ -2,7 +2,7 @@ OcpiXilinxLinuxRepoTag:=xilinx-v2019.2.01
 
 include $(OCPI_CDK_DIR)/include/xilinx/xilinx-rcc-platform-definition.mk
 OcpiCXXFlags+=-fno-builtin-memset -fno-builtin-memcpy
-OcpiCFlags+=-fno-builtin-memset
+OcpiCFlags+=-fno-builtin-memset -fno-builtin-memcpy
 OcpiPlatformOs:=linux
 OcpiPlatformOsVersion:=19_2
 OcpiPlatformArch:=aarch64
