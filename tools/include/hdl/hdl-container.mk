188a12481e7bd2de379c7fc1def7cf8f9ead26bd
Jim Kulp <jek@parera.com>
2020-05-19 14:05:01 +0000

    #934 Fix the matchstiq_z1 build issue where the platform has devices that are not in ProjectDependencies

--- tools/include/hdl/hdl-container.mk
+++ tools/include/hdl/hdl-container.mk
@@ -93,7 +93,17 @@ override HdlLibraries+=sdp axi platform
 # Note that the platform directory should be first XML dir since the config file name should be
 # scoped to the platform.
 override XmlIncludeDirsInternal:=\
-   $(call Unique,$(HdlPlatformDir) $(HdlPlatformDir)/hdl $(XmlIncludeDirs) $(XmlIncludeDirsInternal) $(HdlAssembly))
+   $(call Unique,$(HdlPlatformDir) \
+                 $(HdlPlatformDir)/hdl \
+                 $(HdlPlatformDir)/devices/ \
+                 $(HdlPlatformDir)/devices/hdl \
+                 $(realpath $(HdlPlatformDir))/../../../../exports/lib/devices \
+                 $(realpath $(HdlPlatformDir))/../../../../exports/lib/devices/hdl \
+                 $(realpath $(HdlPlatformDir))/../../../../exports/lib/cards \
+                 $(realpath $(HdlPlatformDir))/../../../../exports/lib/cards/hdl \
+                 $(XmlIncludeDirs) \
+                 $(XmlIncludeDirsInternal) \
+                 $(HdlAssembly))
 # We might be called from an assembly directory, in which case many of the
 # component libraries are passed through to us, but we might be standalone.
 # Thus we add "devices/adapters/cards" and the platforms own libraries.
