7d08f22d9b4b0b7db8d493e9fae416080af886e7
Robert Neuhaus <robert.neuhaus@cnftech.com>
2020-07-06 10:57:11 -0400

    (#1252) Fix generated yaml format

--- .gitlab-ci/yaml_generator.py
+++ .gitlab-ci/yaml_generator.py
@@ -8,67 +8,58 @@ hw_platform = sys.argv[2]
 host_platform = sys.argv[1]
 CI_PROJECT_DIR = os.environ.get('CI_PROJECT_DIR')
 
-yml = { 
-    'stages': [
-        'prereq',
-        'build',
-        'build-primitives-core',
-        'build-primitives',
-        'build-workers',
-        'build-platforms',
-        'build-assemblies',
-        'test'
+yml = {}
+
+yml['stages'] = [
+    'prereq',
+    'build',
+    'build-primitives-core',
+    'build-primitives',
+    'build-workers',
+    'build-platforms',
+    'build-assemblies',
+    'test'
+]
+  
+yml['default'] = {
+    'tags': [
+        'opencpi',
+        '{}'.format(host_platform),
+        'shell'
+    ],
+    'before_script': [
+        'for f in artifacts*.tar; do if [ -f "$f" ]; then tar -xf "$f"; fi; done',
+        'sleep 2',
+        'date "+%Y-%m-%d %H:%M:%S" > timestamp.txt',
+        'sleep 2',
+    ],
+    'after_script': [
+        'if [ ! -f {}/.success ]; then {}/.gitlab-ci/upload-failed-job.sh exit 1 fi'.format(CI_PROJECT_DIR, CI_PROJECT_DIR),
+        'rm -f {}/.success'.format(CI_PROJECT_DIR),
+        'find -not -type d -newerct "$(< timestamp.txt)" > artifacts-$CI_JOB_STAGE-$CI_JOB_ID.txt',
+        'tar -T artifacts-$CI_JOB_STAGE-$CI_JOB_ID.txt -cf artifacts-$CI_JOB_STAGE-$CI_JOB_ID.tar',
+        'for f in artifacts*.tar; do if [ "$f" != "artifacts-$CI_JOB_STAGE-$CI_JOB_ID.tar" ]; then rm -rf "$f"; fi; done',
     ]
 }
 
-cwd = os.path.dirname(os.path.realpath(sys.argv[0]))
-with open(os.path.join(cwd, '{}-{}.yml'.format(host_platform, hw_platform)), 'w+') as f:
-    yaml.dump(yml, f)
-
-yml = {
-    'default': [
-        {
-            'tags': [
-                'opencpi',
-                '{}'.format(host_platform),
-                'shell'
-            ],
-            'before_script': [
-                'for f in artifacts*.tar; do if [ -f "$f" ]; then tar -xf "$f"; fi; done',
-                'sleep 2',
-                'date "+%Y-%m-%d %H:%M:%S" > timestamp.txt',
-                'sleep 2',
-            ],
-            'after_script': [
-                'if [ ! -f {}/.success ]; then {}/.gitlab-ci/upload-failed-job.sh exit 1 fi'.format(CI_PROJECT_DIR, CI_PROJECT_DIR),
-                'rm -f {}/.success'.format(CI_PROJECT_DIR),
-                'find -not -type d -newerct "$(< timestamp.txt)" > artifacts-$CI_JOB_STAGE-$CI_JOB_ID.txt',
-                'tar -T artifacts-$CI_JOB_STAGE-$CI_JOB_ID.txt -cf artifacts-$CI_JOB_STAGE-$CI_JOB_ID.tar',
-                'for f in artifacts*.tar; do if [ "$f" != "artifacts-$CI_JOB_STAGE-$CI_JOB_ID.tar" ]; then rm -rf "$f"; fi; done',
-            ]
-        }
-    ],
-    'install-prereqs': [
-        {'stage': 'prereq'},
-        {
-            'script': [
-                'yum -y distro-sync',
-                './scripts/install-packages.sh',
-                'touch {}/.success"'.format(CI_PROJECT_DIR)
-            ]
-        }
-    ],
-    'build-opencpi': [
-        {'stage': 'build'},
-        {
-            'script': [
-                './scripts/install-prerequisites.sh',
-                'touch {}/.success"'.format(CI_PROJECT_DIR)
-            ]
-        }
-    ],
+yml['install-prereqs'] = {
+    'stage': 'prereq',
+    'script': [
+        'yum -y distro-sync',
+        './scripts/install-packages.sh',
+        'touch {}/.success"'.format(CI_PROJECT_DIR)
+    ]
 }
 
+yml['build-opencpi'] = {
+    'stage': 'build',
+    'script': [
+        './scripts/install-prerequisites.sh',
+        'touch {}/.success"'.format(CI_PROJECT_DIR)
+    ]
+}
+
+cwd = os.path.dirname(sys.argv[0])
 source_cmd = 'source cdk/opencpi-setup.sh -r'
 build_hdl = 'ocpidev build -d {} --hdl-platform {}'
 build_plat = 'ocpidev build hdl platforms -d {} --hdl-platform {}'
@@ -113,17 +104,18 @@ for project in projects:
         else:
             stage = 'build-workers'
 
-        yml['build-{}-{}'.format(os.path.basename(project), os.path.basename(worker))] = [
-            {'stage': stage},
-            {
-                'script': [
-                    source_cmd,
-                    script
-                ]
-            }
-        ]
+        yml['build-{}-{}'.format(os.path.basename(project), os.path.basename(worker))] = {
+            'stage': stage,
+            'script': [
+                source_cmd,
+                script
+            ]
+        }
 
-yml= yaml.dump(yml).replace('\n-', '\n ')
 
-with open(os.path.join(cwd, '{}-{}.yml'.format(host_platform, hw_platform)), 'a') as f:
-    f.write(yml)
+
+with open(os.path.join(cwd, '{}-{}.yml'.format(host_platform, hw_platform)), 'w') as f:
+    yml= yaml.dump(yml, f, width=1000, default_flow_style=False)
+
+# with open(".gitlab-ci/templates.yml", 'r') as stream:
+#     print(yaml.safe_load(stream))
