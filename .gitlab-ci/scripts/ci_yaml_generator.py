fb76653e6c790f2a703c4ccc6a561bbe842be581
robert-neuhaus <robert.neuhaus@cnftech.com>
2020-08-20 18:27:34 -0500

    (#1252) Fix include format
    [ci skip]

--- .gitlab-ci/scripts/ci_yaml_generator.py
+++ .gitlab-ci/scripts/ci_yaml_generator.py
@@ -10,13 +10,12 @@ from ci_utils import ci_project, ci_platform, ci_job, ci_env
 
 
 def main():
-    script_dir = __file__
-    opencpi_path = Path(script_dir, '..', '..', '..').resolve()
+    opencpi_path = Path(__file__, '..', '..', '..').resolve()
     os.chdir(opencpi_path)
-    ci_path = Path('.gitlab-ci')
-    yaml_path = Path(ci_path, 'yaml')
+    gitlab_yml_path = Path('.gitlab-ci')
+    yaml_path = Path(gitlab_yml_path, 'yaml')
     projects_path = Path('projects')
-    host_platform_whitelist_path = Path(ci_path, 'scripts', 
+    host_platform_whitelist_path = Path(gitlab_yml_path, 'scripts', 
                                         'host_platform_whitelist.txt')
 
     with open(host_platform_whitelist_path) as f:
@@ -48,21 +47,29 @@ def main():
                               '{}.yml'.format(cross_platform.name))
             ci_job.dump(cross_jobs, cross_path)
 
-        host_include = [str(path) for path in 
-            Path(yaml_path, host_platform.name).glob('*.yml')]
-        host_jobs = ci_job.make_jobs(stages, host_platform, projects, 
-                                     include=host_include)
+        host_jobs = ci_job.make_jobs(stages, host_platform, projects)
         host_path = Path(yaml_path, '{}.yml'.format(host_platform.name))
         ci_job.dump(host_jobs, host_path)
+        
+        host_include = [str(path) for path in 
+            Path(yaml_path, host_platform.name).glob('*.yml')]
+        host_dict = {'include': host_include}
+        dump(host_dict, host_path, 'a')
 
     print('Updating .gitlab-ci.yml')
-    ci_include = [str(path) for path in Path(yaml_path).glob('*.yml')]
-    ci_dict = {
-        'include': ci_include,
+    gitlab_yml_path = Path('.gitlab-ci.yml')
+    gitlab_yml_include = [str(path) for path in Path(yaml_path).glob('*.yml')]
+    gitlab_yml_dict = {
+        'include': gitlab_yml_include,
         'stages': stages
     }
-    with open('{}.yml'.format(ci_path), 'w+') as yml:
-        yaml.safe_dump(ci_dict, yml, width=1000, default_flow_style=False)
+    dump(gitlab_yml_dict, gitlab_yml_path, 'w+')
+
+
+def dump(yaml_dict, yaml_path, mode):
+    with open(yaml_path, mode) as yml:
+        yaml.safe_dump(yaml_dict, yml, width=1000, default_flow_style=False)
+
 
 if __name__ == '__main__':
     main()
