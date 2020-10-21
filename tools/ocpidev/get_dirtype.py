1bb8c2921948c4ac52b74d58e0b82f8766012470
Cody Jackson <cody.jackson@cnftech.com>
2020-10-21 13:36:03 -0500

    Added need_name() to ocpidev_errors.py Added imports to do_project.py Created get_dirtype.py

---
+++ tools/ocpidev/get_dirtype.py
@@ -0,0 +1,34 @@
+from pathlib import PosixPath
+
+from tools.ocpidev.ocpidev import parse_makefile
+from tools.ocpidev.ocpidev_errors import bad
+
+
+def get_dirtype(*args):
+    """Determine the type of Makefile based on directory type.
+
+    If there is no Makefile or no appropriate line in the Makefile, dirtype is an emtpy string.
+    """
+    dirtype = ""
+    makefile_dir = args[1]
+
+    try:
+        if not isinstance(makefile_dir, PosixPath):  # Argument is not a Path object
+            raise TypeError
+        if not makefile_dir.is_dir():  # Requested directory is not actually a directory
+            raise NotADirectoryError
+        if not makefile_dir:  # Requested directory does not exist
+            raise OSError
+        else:
+            if makefile_dir / "Makefile":
+                dirtype = parse_makefile(makefile_dir / "Makefile")
+            elif makefile_dir / "project-package-id" and not dirtype:
+                dirtype = "project"
+    except TypeError:
+        bad(f"{makefile_dir} is not a Path object")
+    except NotADirectoryError:
+        bad(f"{makefile_dir} should be a directory but is not")
+    except OSError:
+        bad(f"Directory {makefile_dir} should exist and does not")
+
+    return dirtype
