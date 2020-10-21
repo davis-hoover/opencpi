62affbc9c0a21ac17637d64bd29229ec330a581c
Cody Jackson <cody.jackson@cnftech.com>
2020-10-21 14:09:49 -0500

    Added need_name() and try_return_bool() to ocpidev_errors.py Added imports to do_project.py Created dirtypes.py, which includes check_dirtype(), get_dirtype(), and get_subdir() Created parse_makefile.py Added get_project.py Added (de)register_delete_project.py

--- tools/ocpidev/dirtypes.py
+++ tools/ocpidev/dirtypes.py
@@ -1,6 +1,7 @@
 from pathlib import PosixPath
 
-from tools.ocpidev.ocpidev import parse_makefile
+from tools.ocpidev.check_components import check_components
+from tools.ocpidev.ocpidev import parse_makefile, make_library
 from tools.ocpidev.ocpidev_errors import bad
 
 
@@ -47,3 +48,94 @@ def check_dirtype(*args):
         bad(f"{makefile_dir / 'Makefile'} is not correctly formatted. No 'include *.mk' lines.")
     except TypeError:
         bad(f"{makefile_dir / 'Makefile'} has an unexpected dirtype {dirtype}; expected {args[2]}")
+
+
+def get_subdir():
+    """This function sets the subdir variable based on the current directory and whether the library/platform options
+    are set.
+
+    This is for use only when creating assets meant to reside inside libraries. This is NOT for use when
+    creating platforms, assemblies, primitives, libraries, apps, projects, or primitives.
+
+    This function essentially does nothing except call get_dirtype "." when one of those nouns is being operated on.
+    """
+    if noun in ("worker", "device", "spec", "component", "protocol", "properties", "signals", "test", "card", "slot"):
+        libasset = 1
+    elif noun in ("library", "assembly", "assemblies", "platform", "platforms", "primitive", "primitives",
+                  "application", "applications", "project", "registry"):
+        libasset = ""
+    else:
+        bad(f"Invalid noun {noun}")
+
+    dirtype = get_dirtype(".")
+    subdir = "."
+
+    if not libasset or (not standalone and dirtype in project):
+        if not libbase != "hdl" or libbase == hdl.is_dir() or verb != "create":
+            make_hdl_dir()
+        if library:
+            if library in components:
+                subdir = library
+            elif library in ("hdl/cards" or "hdl/devices" or "hdl/adapters"):
+                autocreate = 1
+                subdir = library
+            elif library in "hdl/*":
+                subdir = library
+            else:
+                subdir = f"components/{library}"
+        elif platform:
+            if f"{hdlorrcc}/platforms/{platform}".is_dir():
+                autocreate = 1
+                subdir = f"{hdlorrcc}/platforms/{platform}/devices"
+            else:
+                bad(f"The platform {platform} does not exist in /{hdlorrcc}/platforms/{platform}/")
+        elif libbase == "hdl":
+            autocreate = 1
+            subdir = "hdl/devices"
+        elif project:
+            subdir = "."
+        else:
+            check_components(used)
+            subdir = components
+    elif not libasset or (not standalone and dirtype in library):
+        if library:
+            if platform or card or project:
+                bad(f"Cannot specify -P or -p (platform/project) within a libraries collection")
+            else:
+                subdir = basename(library)
+        else:
+            bad(f"Must specifiy a library when operating from a libraries collection")
+    elif not libasset or (not standalone and dirtype in hdl-platform):
+        if library or platform or card or project:
+            bad(f"Cannot specify '-l, -h, -P, or -p' (library/platform/project) within a platform.")
+        else:
+            autocreate = 1
+            subdir = devices
+    elif not libasset or (not standalone and dirtype in hdl-platforms):
+        if platform:
+            if library or card or project:
+                bad(f"Cannot specify '-l, -h, -P, or -p' (library/platform) within a platform.")
+            if not platform.is_dir():
+                bad(f"The platform {platform} does not exist (in hdl/platforms/{platform})")
+            else:
+                autocreate = 1
+                subdir = platform/devices
+        else:
+            bad(f"Must choose a platform (-P) when operating from the platforms directory.")
+    else:
+        if not dirtype:
+            bad(f"Cannot operate within unknown directory type. Try returning to the top level of your project.")
+        else:
+            bad(f"Cannot operate within directory of type {dirtype}. Try returning to the top level of your project.")
+
+    if subdir.is_dir() or verb != "create" or autocreate == 1:
+        make_library(subdir)
+        subdir
+    if not subdir.is_dir():
+        bad(f"The library for '{library}' '{subdir}' does not exist.")
+
+    subdir_library = get_dirtype(subdir)
+    if standalone or dirtype == library or project:  # Confirm targeted subdirectory is actually a library
+        bad(f"The directory for '{library}' '({libdir})' is not a library but type '{dirtype}'.")
+
+    get_dirtype(".")  # Restore current directory (desired dirtype) for use after this function call
