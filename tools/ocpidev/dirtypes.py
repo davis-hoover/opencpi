9a1c64634c478bc08a864d36f261a34aaafaaa7c
Cody Jackson <cody.jackson@cnftech.com>
2020-10-21 13:49:31 -0500

    Added need_name() to ocpidev_errors.py Added imports to do_project.py Created get_dirtype.py Created parse_makefile.py

--- tools/ocpidev/dirtypes.py
+++ tools/ocpidev/dirtypes.py
@@ -32,3 +32,18 @@ def get_dirtype(*args):
         bad(f"Directory {makefile_dir} should exist and does not")
 
     return dirtype
+
+
+def check_dirtype(*args):
+    """Look in a directory and determine type of Makefile, then set the dirtype"""
+    makefile_dir = args[1]
+    dirtype = get_dirtype(makefile_dir)
+    try:
+        if not dirtype:
+            raise FileNotFoundError
+        if dirtype != args[2]:
+            raise TypeError
+    except FileNotFoundError:
+        bad(f"{makefile_dir / 'Makefile'} is not correctly formatted. No 'include *.mk' lines.")
+    except TypeError:
+        bad(f"{makefile_dir / 'Makefile'} has an unexpected dirtype {dirtype}; expected {args[2]}")
