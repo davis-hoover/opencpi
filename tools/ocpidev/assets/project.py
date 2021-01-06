073869ef2ef30136021f8ddc6ee87c621d5478ac
Rory Foster <rory.foster@cnftech.com>
2021-01-05 22:57:20 -1000

    remove Project.mk

--- tools/ocpidev/assets/project.py
+++ tools/ocpidev/assets/project.py
@@ -1123,9 +1123,6 @@ class Project(RunnableAsset, RCCBuildableAsset, HDLBuildableAsset, ShowableAsset
         ocpiutil.write_file_from_string( proj_dir + "/.gitattributes", ocpitemplate.PROJ_GIT_ATTR)
         template = jinja2.Template(ocpitemplate.PROJ_MAKEFILE, trim_blocks=True)
         ocpiutil.write_file_from_string( proj_dir + "/Makefile", template.render(**template_dict))
-        # TODO: Once traditional XML is used, delete the next two lines
-        template = jinja2.Template(ocpitemplate.PROJ_PROJECT_MK, trim_blocks=True)
-        ocpiutil.write_file_from_string( proj_dir + "/Project.mk", template.render(**template_dict))
         # TODO: For traditional XML, replace PROJ_PROJECT_XML_LEGACY with PROJ_PROJECT_XML
         template = jinja2.Template(ocpitemplate.PROJ_PROJECT_XML_LEGACY, trim_blocks=True)
         ocpiutil.write_file_from_string( proj_dir + "/Project.xml", template.render(**template_dict))
