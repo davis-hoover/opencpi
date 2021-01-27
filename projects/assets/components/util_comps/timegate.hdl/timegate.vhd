0fdfeab8dc5b182924925e9d55ff833189034692
jdarko <jerry.darko@cnftech.com>
2021-01-27 16:14:55 -0500

    #1902 Fix bugs and add improvments to timegate, fix sdp_send bug, add on_off opcode logic to qdac, and fix typo in qdac_ad9361_sub

--- projects/assets/components/util_comps/timegate.hdl/timegate.vhd
+++ projects/assets/components/util_comps/timegate.hdl/timegate.vhd
@@ -15,14 +15,13 @@
 --
 -- You should have received a copy of the GNU Lesser General Public License
 -- along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 library IEEE; use IEEE.std_logic_1164.all; use ieee.numeric_std.all;
 library ocpi; use ocpi.types.all, ocpi.wci.all, ocpi.util.all;
 library cdc; use cdc.cdc.all;
 architecture rtl of worker is
   constant time_width_c        : natural := ulonglong_t'length;
   constant nchunks_c           : natural := max(in_in.data'length, time_width_c)/in_in.data'length;
-  signal time_late_r           : bool_t;      -- timestamp arrived after its time
-  signal error_r               : bool_t;
   -- The time gate state machine
   type state_t is (open_e,         -- gate is open, samples are flowing
                    time_coming_e,  -- time is coming in chunks, not yet complete
@@ -64,8 +63,18 @@ architecture rtl of worker is
   signal eom                   : bool_t;
   signal valid                 : bool_t;
   signal give                  : bool_t;
+  -- status signals
+  signal error_r               : bool_t;
+  signal time_late_r           : bool_t;    -- timestamp arrived after its time
+  
+  signal clr_late_time_sticky_written   : bool_t;
+  signal clr_late_time_sticky           : bool_t;
+  signal time_delta            : ulonglong_t;
 
 begin
+  -- TODO: Implement ignore_time_stamps
+  
+  props_out.actual_time_to_requested_time_delta <= time_delta;
   good_opcode <= to_bool(in_in.opcode = ComplexShortWithMetadata_samples_op_e or
                          in_in.opcode = ComplexShortWithMetadata_time_op_e or 
                          in_in.opcode = ComplexShortWithMetadata_flush_op_e);
@@ -76,16 +85,17 @@ begin
   fifo_in_encoded_bits <= "00" when (its(in_in.eof)) else
                           "01" when (in_in.opcode = ComplexShortWithMetadata_samples_op_e) else
                           "10" when (in_in.opcode = ComplexShortWithMetadata_time_op_e) else
-                          "11" when (in_in.opcode = ComplexShortWithMetadata_flush_op_e);
+                          "11" when (in_in.opcode = ComplexShortWithMetadata_flush_op_e) else 
+                          "01";
 
   fifo_in               <= fifo_in_encoded_bits & in_in.eom & in_in.data;
   fifo_out_data         <= fifo_out(in_in.data'range);
   fifo_out_encoded_bits <= fifo_out(fifo_out'left downto fifo_out'left-1);
   fifo_out_is_eom       <= to_bool(fifo_out(fifo_out'left-2));
-  fifo_out_is_eof       <= '1' when (fifo_out_encoded_bits = "00") else '0';
-  fifo_out_is_samples   <= '1' when (fifo_out_encoded_bits = "01") else '0';
-  fifo_out_is_time      <= '1' when (fifo_out_encoded_bits = "10") else '0';
-  fifo_out_is_flush     <= '1' when (fifo_out_encoded_bits = "11") else '0';
+  fifo_out_is_eof       <= to_bool(fifo_out_encoded_bits = "00");
+  fifo_out_is_samples   <= to_bool(fifo_out_encoded_bits = "01");
+  fifo_out_is_time      <= to_bool(fifo_out_encoded_bits = "10");
+  fifo_out_is_flush     <= to_bool(fifo_out_encoded_bits = "11");
   fifo_enq              <= fifo_full_n and ((in_in.ready and good_opcode) or in_in.eof);
   fifo_deq              <= to_bool(fifo_empty_n and not its(fifo_out_is_eof) and out_in.ready and
                                   (state_r = open_e or fifo_out_is_time));
@@ -101,12 +111,17 @@ begin
   out_out.data   <= fifo_out_data;
   out_out.som    <= som;
   out_out.eom    <= eom;
-  out_out.eof    <= fifo_out_is_eof and fifo_full_n;
+  out_out.eof    <= fifo_out_is_eof and fifo_empty_n;
   out_out.valid  <= valid;
   out_out.give   <= give;
   out_out.opcode <= ComplexShortWithMetadata_flush_op_e when (its(fifo_out_is_flush) and its(give))  else
                     ComplexShortWithMetadata_samples_op_e;
   
+  ctl_out.attention <= error_r;
+  ctl_out.error <= to_bool((ctl_in.control_op = START_e) and
+                   its(props_in.force_error_on_invalid_time_at_start) and
+                   its(time_in.valid));
+
   fifo : cdc.cdc.fifo
     generic map(
       WIDTH       => in_in.data'length + 3, -- MSB is encoded bits
@@ -121,22 +136,56 @@ begin
       dst_DEQ     => fifo_deq,
       dst_out     => fifo_out,
       dst_EMPTY_N => fifo_empty_n);
+  
+  clr_late_time_sticky_written  <= props_in.clr_late_time_sticky_written and props_in.clr_late_time_sticky and ctl_in.is_operating;
+  pulse : cdc.cdc.pulse
+    port map(src_clk  => ctl_in.clk,
+              src_rst => ctl_in.reset,
+              src_in  => clr_late_time_sticky_written,
+              src_rdy => open,
+              dst_clk => out_in.clk,
+              dst_rst => out_in.reset,
+              dst_out => clr_late_time_sticky);
+  
+  time_late : component cdc.cdc.fast_pulse_to_slow_sticky
+  port map(
+    fast_clk    => out_in.clk,
+    fast_rst    => out_in.reset,
+    fast_pulse  => time_late_r,
+    slow_clk    => ctl_in.clk,
+    slow_rst    => ctl_in.reset,
+    slow_sticky => props_out.late_time_sticky,
+    slow_clr    => clr_late_time_sticky_written);
+          
 
   notchunked: -- simpler version when time is not chunked
   if in_in.data'length >= time_width_c generate
     g0 : process(out_in.clk)
     begin
-      if its(out_in.reset) then
-        time_late_r <= bfalse;
-        error_r     <= bfalse;
-        state_r     <= open_e; -- gate is initially open
-      elsif rising_edge(out_in.clk) and fifo_empty_n then
-        if its(fifo_out_is_time) then
-          time_to_transmit_r <= unsigned(fifo_out_data(time_width_c-1 downto 0))
-                                - props_in.time_correction;
-          state_r <= time_waiting_e;
-        elsif state_r = time_waiting_e and time_now >= time_to_transmit_r then
-          state_r <= open_e;
+      if rising_edge(out_in.clk) then 
+        if its(out_in.reset) then
+          time_late_r <= bfalse;
+          error_r     <= bfalse;
+          state_r     <= open_e; -- gate is initially open
+          time_delta  <= (others => '0');
+        elsif its(fifo_empty_n) then
+          if its(fifo_out_is_time) then
+            time_to_transmit_r <= unsigned(fifo_out_data(time_width_c-1 downto 0))
+                                  - props_in.time_correction;
+            state_r <= time_waiting_e;
+          elsif state_r = time_coming_e then -- data opcode
+            state_r <= error_e; 
+          elsif state_r = time_waiting_e and time_now >= time_to_transmit_r then
+            state_r <= open_e;
+            time_delta <= time_now - time_to_transmit_r;
+            if time_now > time_to_transmit_r then
+              time_late_r <= btrue;
+            else
+              time_late_r <= bfalse;
+            end if;
+          elsif state_r = error_e then -- non-time opcode when waiting for time chunks
+            error_r <= btrue;
+          end if;
         end if;
       end if;
     end process; -- end of process
@@ -151,32 +200,42 @@ begin
     -- The state machine between getting time, waiting for time, and passing samples
     input_chunked : process(out_in.clk)
     begin
-      if its(out_in.reset) then
-        time_late_r <= bfalse;
-        error_r     <= bfalse;
-        state_r     <= open_e; -- gate is initially open
-      elsif rising_edge(out_in.clk) and fifo_empty_n then
-        if its(fifo_out_is_time) then -- time opcode
-          if state_r = open_e or state_r = time_waiting_e then
-            time_chunks_r(0) <= unsigned(fifo_out_data);
-            state_r <= time_coming_e;
-            time_chunk_idx_r <= to_unsigned(1, time_chunk_idx_r'length);
-          elsif state_r = time_coming_e then -- never will happen if width >= time_width
-            if time_chunk_idx_r = to_unsigned(nchunks_c - 1, time_chunk_idx_r'length) then
-              time_to_transmit_r <= (unsigned(fifo_out_data) & time_chunks) - props_in.time_correction;
-              state_r <= time_waiting_e;
-            else
-              time_chunks_r(to_integer(time_chunk_idx_r)) <= unsigned(fifo_out_data);
-              time_chunk_idx_r <= time_chunk_idx_r + 1;
+      if rising_edge(out_in.clk) then 
+        if its(out_in.reset) then
+          time_late_r <= bfalse;
+          error_r     <= bfalse;
+          state_r     <= open_e; -- gate is initially open
+          time_delta  <= (others => '0');
+        elsif its(fifo_empty_n)  then
+          if its(fifo_out_is_time) then -- time opcode
+            if state_r = open_e or state_r = time_waiting_e then
+              time_chunks_r(0) <= unsigned(fifo_out_data);
+              state_r <= time_coming_e;
+              time_chunk_idx_r <= to_unsigned(1, time_chunk_idx_r'length);
+            elsif state_r = time_coming_e then -- never will happen if width >= time_width
+              if time_chunk_idx_r = to_unsigned(nchunks_c - 1, time_chunk_idx_r'length) then
+                time_to_transmit_r <= (unsigned(fifo_out_data) & time_chunks) - props_in.time_correction;
+                state_r <= time_waiting_e;
+              else
+                time_chunks_r(to_integer(time_chunk_idx_r)) <= unsigned(fifo_out_data);
+                time_chunk_idx_r <= time_chunk_idx_r + 1;
+              end if;
             end if;
+          elsif state_r = time_coming_e then -- data opcode
+            state_r <= error_e; 
+          elsif state_r = time_waiting_e and time_now >= time_to_transmit_r then
+            state_r <= open_e;
+            time_delta <= time_now - time_to_transmit_r;
+            if time_now > time_to_transmit_r then
+              time_late_r <= btrue;
+            else
+              time_late_r <= bfalse;
+            end if;
+          elsif state_r = error_e then -- non-time opcode when waiting for time chunks
+            error_r <= btrue;
           end if;
-        elsif state_r = time_coming_e then -- data opcode
-          state_r <= error_e; -- non-time opcode when waiting for time chunks
-          error_r <= btrue;
-        elsif state_r = time_waiting_e and time_now >= time_to_transmit_r then
-          state_r <= open_e;
-        end if;
-      end if;    -- end of rising edge and fifo not empty
+        end if;    -- end of fifo not empty
+      end if;
     end process; -- end of process
   end generate;
 end rtl;
