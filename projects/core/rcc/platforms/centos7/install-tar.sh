207801e0ca296c840d42b7da493b8c23672f8636
Aaron Olivarez <aaron@olivarez.info>
2021-06-25 13:13:14 -0500

    add ubuntu16.04 packages and centos7 tar install

---
+++ projects/core/rcc/platforms/centos7/install-tar.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# This file is protected by Copyright. Please refer to the COPYRIGHT file
+# distributed with this source distribution.
+#
+# This file is part of OpenCPI <http://www.opencpi.org>
+#
+# OpenCPI is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# OpenCPI is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
+[ -z "$OCPI_CDK_DIR" ] && echo 'Environment variable OCPI_CDK_DIR not set' && exit 1
+
+target_platform="$1"
+name=tar
+version=1.32 # latest as of 06/23/2021
+pkg_name="$name-$version"
+dl_url="https://ftp.gnu.org/gnu/$name/$pkg_name.tar.gz"
+extracted_dir="$pkg_name"
+cross_build=0 # disable until needed. Update `build/places` when enabled. 
+
+# Download and extract source
+source "$OCPI_CDK_DIR/scripts/setup-prerequisite.sh" \
+       "$target_platform" \
+       "$name" \
+       "$description" \
+       "${dl_url%/*}" \
+       "${dl_url##*/}" \
+       "$extracted_dir" \
+       "$cross_build" 
+
+../configure
+make 
+sudo make install 
