ca6e3575d618165fa4f9212f8a65768812d4239b
Bob Tracy <rct@frus.com>
2021-08-25 10:38:57 -0500

    Merge branch '930-update-tutorials-as-required-for-make-less-development' into 'release-2.3.0'
    Resolve "Update tutorials as required for make-less development"
    
    Closes #930
    
    See merge request opencpi/opencpi!684
    (cherry picked from commit fee513d6c30b5f58d81ddbe8af4bf4f123b399e0)
    
    Conflicts:
    	doc/tutorials/Tutorial_1_Component_Based_Development_Intro-cg.fodt
    	doc/tutorials/Tutorial_2_Design_HDL_Assemblies-cg.fodt
    	doc/tutorials/Tutorial_3_Develop_RCC_Worker-cg.fodt
    	doc/tutorials/Tutorial_4_Third_Party_Libs_in_RCC_Workers-cg.fodt

--- projects/tutorial/components/complex_mixer.rcc/complex_mixer.cc
+++ projects/tutorial/components/complex_mixer.rcc/complex_mixer.cc
@@ -1,31 +1,10 @@
 /*
- * This file is protected by Copyright. Please refer to the COPYRIGHT file
- * distributed with this source distribution.
- *
- * This file is part of OpenCPI <http://www.opencpi.org>
- *
- * OpenCPI is free software: you can redistribute it and/or modify it under the
- * terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * OpenCPI is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
-
-/*
- * THIS FILE WAS ORIGINALLY GENERATED ON Wed Mar 30 09:48:42 2016 EDT
+ * THIS FILE WAS ORIGINALLY GENERATED ON Sat Aug 14 13:57:10 2021 EDT
  * BASED ON THE FILE: complex_mixer.xml
  * YOU *ARE* EXPECTED TO EDIT IT
  *
  * This file contains the implementation skeleton for the complex_mixer worker in C++
  */
-
 #include "complex_mixer-worker.hh"
 #include <liquid/liquid.h>
 #include <cmath>
@@ -60,12 +39,6 @@ class Complex_mixerWorker : public Complex_mixerWorkerBase
 
   RCCResult run(bool /*timedout*/)
   {
-	// determine if end of file
-	if (in.eof()) {
-	  out.setEOF();
-	  return RCC_DONE;
-	}
-
     const IqstreamIqData* inData = in.iq().data().data();
     IqstreamIqData* outData = out.iq().data().data();
     const size_t num_of_elements = in.iq().data().size(); // size in IqstreamIqData units
@@ -85,7 +58,6 @@ class Complex_mixerWorker : public Complex_mixerWorkerBase
       {
         in_sample.real = Uscale(inData->I);
         in_sample.imag = Uscale(inData->Q);
-
         nco_crcf_step(q);
         nco_crcf_mix_down(q, in_sample, &out_sample);
         outData->I = Scale(out_sample.real);
--- projects/tutorial/components/peak_detector.rcc/peak_detector.cc
+++ projects/tutorial/components/peak_detector.rcc/peak_detector.cc
@@ -1,48 +1,27 @@
 /*
- * This file is protected by Copyright. Please refer to the COPYRIGHT file
- * distributed with this source distribution.
- *
- * This file is part of OpenCPI <http://www.opencpi.org>
- *
- * OpenCPI is free software: you can redistribute it and/or modify it under the
- * terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * OpenCPI is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
-
-/*
- * THIS FILE WAS ORIGINALLY GENERATED ON Tue Mar 29 16:29:51 2016 EDT
+ * THIS FILE WAS ORIGINALLY GENERATED ON Sat Aug 14 09:44:59 2021 EDT
  * BASED ON THE FILE: peak_detector.xml
  * YOU *ARE* EXPECTED TO EDIT IT
  *
  * This file contains the implementation skeleton for the peak_detector worker in C++
  */
-
 #include <algorithm>
-#include <limits>
 #include "peak_detector-worker.hh"
-
 using namespace OCPI::RCC; // for easy access to RCC data types and constants
 using namespace Peak_detectorWorkerTypes;
 
 class Peak_detectorWorker : public Peak_detectorWorkerBase {
-  // Reset these at start(), rather than upon construction,
-  // so that the worker can be restarted with fresh min/max values
-  RCCResult start() {
-    properties().max_peak = std::numeric_limits<int16_t>::min();
-    properties().min_peak = std::numeric_limits<int16_t>::max();
+
+  int16_t max_buff, min_buff;
+
+  RCCResult start(){
+    max_buff = -32768;
+    min_buff = 32767;
     return RCC_OK;
   }
 
   RCCResult run(bool /*timedout*/) {
+
     // 1. Make sure there is room on the output port
     const size_t num_of_elements = in.iq().data().size();
     out.iq().data().resize(num_of_elements);
@@ -52,11 +31,12 @@ class Peak_detectorWorker : public Peak_detectorWorkerBase {
     const IqstreamIqData *idata = in.iq().data().data();
     IqstreamIqData *odata = out.iq().data().data();
     for (unsigned n = num_of_elements; n; --n) {
-      properties().max_peak = std::max(std::max(idata->I,idata->Q), properties().max_peak);
-      properties().min_peak = std::min(std::min(idata->I,idata->Q), properties().min_peak);
-
+      max_buff = std::max(std::max(idata->I,idata->Q),max_buff);
+      min_buff = std::min(std::min(idata->I,idata->Q),min_buff);
       *odata++ = *idata++; // copy this message to output buffer
     }
+    properties().max_peak = max_buff;
+    properties().min_peak = min_buff;
 
     // 3. Advance ports
     return RCC_ADVANCE;
@@ -67,3 +47,4 @@ PEAK_DETECTOR_START_INFO
 // Insert any static info assignments here (memSize, memSizes, portInfo)
 // e.g.: info.memSize = sizeof(MyMemoryStruct);
 PEAK_DETECTOR_END_INFO
+
