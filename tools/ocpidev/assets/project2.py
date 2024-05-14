e263ac651dcc29a11cc3a3a46e3f0d5cc293f4b0
Davis Hoover <dhoover@ceres.office.geontechnologies.com>
2024-05-13 21:06:58 -0400

    fix ocpidev2 build dependency issue

--- tools/ocpidev/assets/project2.py
+++ tools/ocpidev/assets/project2.py
@@ -318,7 +318,7 @@ class Project(SpecsDirectory, Discoverer, AssetBase):
 
     def get_hdl_worker_dependent_libraries(self, comp_library, worker):
         ret = self.get_built_in_hdl_libraries()
-        for lib in comp_library.hdl_libraries:
+        for lib in comp_library.attrs['HdlLibraries']:
             ret.append(lib)
         for lib in worker.attrs['Libraries']:
             ret.append(lib)
