2f43c5fbc8fb3ce004e46d2730d6c9d910784a22
Davis Hoover <dhoover@ceres.office.geontechnologies.com>
2024-07-31 10:08:51 -0400

    fix ocpidev2 bugs

--- tools/ocpidev2/assets/registry2.py
+++ tools/ocpidev2/assets/registry2.py
@@ -879,7 +879,8 @@ class ProjectRegistry():
                             (rcc_platforms2 == [])):
                         gmtstr = gnu_make_target_str
                         makefile.rules['all'].prerequisites.append(gmtstr)
-                        makefile = self.append_asset_rules_to_makefile_variable(
+                        makefile = \
+                            self.append_asset_rules_to_makefile_variable(
                             asset, hdl_targets2, hdl_platforms2,
                             rcc_platforms2, makefile, gnu_make_target_str,
                             tool)
